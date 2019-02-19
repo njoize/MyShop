b622103fdfaba9e8c3544bb632f69ce62681f0a2
njoize <njoize@gmail.com>
2019-02-19 23:31:20 +0700

    Check Space OK

--- app/src/main/java/njoize/dkh/th/co/myshop/MainFragment.java
+++ app/src/main/java/njoize/dkh/th/co/myshop/MainFragment.java
@@ -2,10 +2,13 @@ package njoize.dkh.th.co.myshop;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -18,6 +21,40 @@ public class MainFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Login Controller
+        loginController();
+
+
+    } // Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText usernameEditText = getView().findViewById(R.id.edtUsername);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String usernameString = usernameEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+                MyAlertDialog myAlertDialog = new MyAlertDialog(getActivity());
+
+                if (usernameString.isEmpty() || passwordString.isEmpty()) {
+//                    Have Space
+                    myAlertDialog.normalDialog("ข้อมูลไม่ครบถ้วน","กรุณากรอก Username และ Password");
+
+                } else {
+//                    No Space
+                }
+
+            }
+        });
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
---
+++ app/src/main/java/njoize/dkh/th/co/myshop/ServiceActivity.java
@@ -0,0 +1,13 @@
+package njoize.dkh.th.co.myshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service);
+    }
+}
