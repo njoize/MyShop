ca3339d24c79edf61f832b8e24baa6f931e354ab
njoize <njoize@gmail.com>
2019-02-19 17:30:23 +0700

    Initial commit

---
+++ app/src/main/java/njoize/dkh/th/co/myshop/MainActivity.java
@@ -0,0 +1,22 @@
+package njoize.dkh.th.co.myshop;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Add Fragment to Activity
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.contentMainFragment, new MainFragment())
+                .commit();
+
+
+    }
+}
