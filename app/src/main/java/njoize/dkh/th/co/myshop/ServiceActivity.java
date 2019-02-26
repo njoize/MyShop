46238dcdad6a454ce57bfceb668e683f1074d7d9
njoize <njoize@gmail.com>
2019-02-26 17:47:50 +0700

    Drawer Menu and Hamburger Success

--- app/src/main/java/njoize/dkh/th/co/myshop/ServiceActivity.java
+++ app/src/main/java/njoize/dkh/th/co/myshop/ServiceActivity.java
@@ -1,13 +1,99 @@
 package njoize.dkh.th.co.myshop;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ServiceActivity extends AppCompatActivity {
 
+    //    Explicit
+    private MyConstant myConstant = new MyConstant();
+    private String nameString;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+//        Get ValueLogin
+        getValueLogin();
+
+//        Create Toolbar
+        createToolbar();
+
+    } // Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate();
+        return super.onCreateOptionsMenu(menu);
+
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        actionBarDrawerToggle.syncState();
+    }
+
+    private void getValueLogin() {
+        MyConstant myConstant = new MyConstant();
+
+        SharedPreferences sharedPreferences = getSharedPreferences(myConstant.getSharePreferFileUserLogin(), Context.MODE_PRIVATE);
+        String jsonString = sharedPreferences.getString("JSON", "");
+
+        nameString = sharedPreferences.getString("Name", "");
+    }
+
+    private void createToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbarService);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(myConstant.getNameShopString());
+        getSupportActionBar().setSubtitle("Login by : " + nameString);
+
+        drawerLayout = findViewById(R.id.layoutDrawer);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                ServiceActivity.this,
+                drawerLayout,
+                R.string.open,
+                R.string.close);
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.ic_action_hamberger);
+
+
+    }
+} // Main Class
