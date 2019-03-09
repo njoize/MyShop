77554cc15717d52b59f196aad98410255a6dad22
njoize <njoize@gmail.com>
2019-03-09 20:53:37 +0700

    Member Search View OK

---
+++ app/src/main/java/njoize/dkh/th/co/myshop/NameMemberModel.java
@@ -0,0 +1,14 @@
+package njoize.dkh.th.co.myshop;
+
+public class NameMemberModel {
+
+    private String nameMemberString;
+
+    public NameMemberModel(String nameMemberString) {
+        this.nameMemberString = nameMemberString;
+    }
+
+    public String getNameMemberString() {
+        return nameMemberString;
+    }
+}
